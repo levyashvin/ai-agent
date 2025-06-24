7825ef3cd5c1ee46957b78115b36304af88486d8
levyashvin <yuvayashvin@gmail.com>
2025-06-24 17:23:30 +0530

    Hardcoded version working

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+# Loading api keys
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+api_key = os.environ.get('GEMINI_API_KEY')
 
+# Creating a gemini client
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
+# Test response
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+)
+print(response.text)
+# Response's meta data
+print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+print("Response tokens:", response.usage_metadata.candidates_token_count)
